fbeb044cda2ad5fb854acf055cf9c4059e22f42f
lilian-tsai <lilian.tsai@edgentauems.com.tw>
2024-05-27 10:57:27 +0800

    vite config.js 添加 base

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/vite-deploy-sample/', // put your github repository name
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
